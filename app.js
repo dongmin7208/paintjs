2e4450059395502b13af379b417608e5b6733e77
dongmin7208 <sayg3o3d@gmail.com>
2021-06-25 16:53:06 +0900

    Add addEventListener

---
+++ app.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById("jsCanvas");
+
+function onMouseMove(event) {
+    console.log(event);
+}
+if (canvas) {
+    canvas.addEventListener("mousemove", onMouseMove);
+}
